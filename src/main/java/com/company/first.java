d8e30387eb8b1f6c787f57701167d61ce5901038
K0leg <olegkovtun2005ua@gmail.com>
2024-05-30 13:19:15 +0300

    Add files via upload

---
+++ src/main/java/com/company/first.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class first {
+
+    public static void main(String[] args) {
+
+        System.out.println((int) Math.round(Math.random() * 40 - 20));
+
+        //натуральні числа
+        System.out.println((int) Math.abs(Math.round(Math.random() * 40 - 20)));
+
+
+        //перевірив виводить від -20 до 20
+
+    }
+}
